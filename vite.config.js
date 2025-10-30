40f53d60fc5ef45bbb2d0d3e11624613109d02a5
Jacob Michelsen <chromegadget@gmail.com>
2025-10-30 15:03:09 +0100

    Access dev server on local network

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // or use '0.0.0.0' to expose on all network interfaces
+    port: 5173, // default port
+  }
 })
